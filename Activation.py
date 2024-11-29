fddb704ac9d045a5350b009d28ba9a2bc69f7f80
mohamedvpn <103552750+mohamedvpn@users.noreply.github.com>
2024-11-29 17:35:27 +0200

    Update Activation.py

--- Activation.py
+++ Activation.py
@@ -1,3 +1,3 @@
 {
-  "6JJ8-DGGR-3ZD4-YTPL":{'name':'Ali Ahmed','mac':'e4a3da37ff5f9b92d209509d19b5579b09347c1e4b401f10c5c7bc2c4c8dbc3ed8d6S4n0'}
+  "6JJ8-DGGR-3ZD4-YTPL":{'name':'Ali Ahmed','mac':'943dbca9e2c09d1dfaf7ce046ee330b472b8482868d7faf49569334091ccfbf0d8d6S4n0'}
 }
